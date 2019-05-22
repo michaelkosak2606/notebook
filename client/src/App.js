e8af8616a3aeaafc7289c0b353149d52cdbfe2d6
Michael Kosak <michaelkosak2606@gmail.com>
2019-05-22 22:02:49 +0200

    added structure and react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard'
+import Edit from './components/Edit'
+import Create from './components/Create'
+import Header from './components/Header'
 import "./scss/App.scss"
 
 
-function App() {
-  return (
-    <div> Hello</div>
-  );
+
+
+class App extends Component {
+  render() {
+
+    return (
+      <Router>
+        <div className="container">
+          <Header />
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/edit/:id" exact component={Edit} />
+          <Route path="/create" exact component={Create} />
+        </div>
+      </Router >
+
+    );
+  }
 }
 
 export default App;
